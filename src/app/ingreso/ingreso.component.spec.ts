3beb3a0b886ab5928f1265e4b2fa8fc554c1e291
WillDev <wilmar.garciava@gmail.com>
2023-10-05 23:16:22 -0500

    feat(test) Se añaden test unitarios para modulo de ingreso

--- src/app/ingreso/ingreso.component.spec.ts
+++ src/app/ingreso/ingreso.component.spec.ts
@@ -1,15 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IngresoComponent } from './ingreso.component';
+import { ClienteService } from '../service/cliente.service';
 
 describe('IngresoComponent', () => {
   let component: IngresoComponent;
   let fixture: ComponentFixture<IngresoComponent>;
+  let mockClienteService: any;
+
+  beforeEach(async () => {
+    mockClienteService = jasmine.createSpyObj('ClienteService', ['setCliente']);
+
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      declarations: [IngresoComponent],
+      providers: [{ provide: ClienteService, useValue: mockClienteService }]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [IngresoComponent]
-    });
     fixture = TestBed.createComponent(IngresoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +28,26 @@ describe('IngresoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have empty tipo and numero on init', () => {
+    expect(component.tipo).toBe('');
+    expect(component.numero).toBe('');
+  });
+
+  it('should have pattern set to cedula format initially', () => {
+    expect(component.pattern).toBe("[0-9]{8,11}");
+  });
+
+  it('should change pattern when tipo changes to pasaporte', () => {
+    component.tipo = 'pasaporte';
+    component.changeValidation();
+    expect(component.pattern).toBe("[A-Za-z0-9]+");
+  });
+
+  it('should change pattern back to cedula when tipo changes to cedula', () => {
+    component.tipo = 'cedula';
+    component.changeValidation();
+    expect(component.pattern).toBe("[0-9]{8,11}");
+  });
 });
+
