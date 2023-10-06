b8a9dccf682c6f313c0b31e63bd060a0e577a45c
WillDev <wilmar.garciava@gmail.com>
2023-10-05 20:56:24 -0500

    feat(frontend): Se inicializa el proyecto angular con Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresoComponent } from './ingreso/ingreso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'ingreso', component: IngresoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { ResumenComponent } from './resumen/resumen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresoComponent,
+    ResumenComponent
   ],
   imports: [
     BrowserModule,
